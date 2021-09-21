bf2b5f64af7db603a4617ec79e23c817936ad6f6
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2021-09-21 21:41:57 +0900

    add crete read update delete

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,35 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(title: params[:title],
+                    content: params[:content])
+    if @post.save
+      flash[:notice] = "投稿しました"
+      redirect_to("/posts/index")
+    else
+      render("/posts/new")
+    end
+  end
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.title = params[:title]
+    @post.content = params[:content]
+    if @post.save
+      flash[:notice] = "投稿を編集しました"
+      redirect_to("/posts/index")
+    else
+      render("/posts/edit")
+    end
+  end
+end
