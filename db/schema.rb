979ad1cae3e641713d0212a2948aa55e226bb3d4
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2021-09-22 23:51:34 +0900

    add CRUD in users

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_21_113223) do
+ActiveRecord::Schema.define(version: 2021_09_22_140907) do
 
   create_table "posts", force: :cascade do |t|
     t.text "title"
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
